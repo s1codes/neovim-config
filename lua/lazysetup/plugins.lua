98fd531e820eea8a1312cb0679234c4c069ed95e
s1codes <banerjeesayantan1609@gmail.com>
2023-10-28 13:08:19 +0530

    Added new treesitter languages

--- lua/lazysetup/plugins.lua
+++ lua/lazysetup/plugins.lua
@@ -5,7 +5,7 @@ local plugins = {
 		config = function ()
 			local configs = require("nvim-treesitter.configs")
 			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "go", "svelte" },
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "go", "svelte", "json", "jsdoc", "typescript", "tsx" },
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },
