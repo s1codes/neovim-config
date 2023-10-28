97693696ba2ba0c5c90935c35720a34b3994f678
s1codes <banerjeesayantan1609@gmail.com>
2023-10-28 13:02:26 +0530

    Fixed tree-sitter highlight not working

--- lua/lazysetup/plugins.lua
+++ lua/lazysetup/plugins.lua
@@ -1,4 +1,17 @@
-plugins = {
+local plugins = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function ()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "go", "svelte" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end
+	},
 	{
 		"nvim-telescope/telescope.nvim", tag = '0.1.4',
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -11,23 +24,9 @@ plugins = {
 			}
 		}
 	},
-	{ 
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build" 
-	},
 	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function ()
-			local configs = require("nvim-treesitter.configs")
-
-			configs.setup {
-				ensure_installed = { "lua", "go", "javascript", "vim", "vimdoc", "html", "css", "svelte" },
-				sync_install = false, 
-				highlight = { enabled = true },
-				indent = { enable = true },
-			}
-		end
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
 	},
 	{
 		"navarasu/onedark.nvim",
